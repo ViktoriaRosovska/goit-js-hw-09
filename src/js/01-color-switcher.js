767632311923d56671bf7d9fbfb4c83d1616f1df
Viktoria Rosovska <torimeister@gmail.com>
2023-05-21 18:52:51 +0300

    solve 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+startBtn.addEventListener('click', onStartButtonChangeBg);
+stopBtn.addEventListener('click', onStopButtonCancelChangeBg);
+
+
+function onStartButtonChangeBg() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+    }, 1000)
+}
+
+function onStopButtonCancelChangeBg() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
